c5b6b0b36215bfaf773fd63e6a17c55040778f0d
Ingy döt Net <ingy@ingy.net>
2012-10-25 12:43:42 -0700

    New Moos goodness.

--- lib/Pegex/Base.pm
+++ lib/Pegex/Base.pm
@@ -1,111 +1,251 @@
 # Pegex::Base generated from Moos.pm
 
+# The entire implementation of Pegex::Base (and all its related classes)
+# are defined inside this one file.
 use strict;
 use warnings;
 
 package Pegex::Base;
+use v5.10.0;
+use mro;
+use Scalar::Util;
 
-# our $VERSION = '0.03';
+# our $VERSION = '0.05';
 
 sub import {
-    my $caller = caller;
-    no strict 'refs';
-    no warnings 'once';
+    # Get name of the "class" from whence "use Pegex::Base;"
+    my $package = caller;
+
+    # Turn on strict/warnings for caller
     strict->import;
     warnings->import;
-    ${"$caller\::__meta"} = { has => [] };
-    *{"$caller\::has"} = sub {
-        my ($name, %args) = @_;
-        my $meta = ${"$caller\::__meta"};
-        push @{$meta->{has}}, [$name, \%args];
-        my ($builder, $default) = @args{qw(builder default)};
 
-        my $method =
-            $builder ? sub {
-                $#_ ? $_[0]{$name} = $_[1] :
-                exists($_[0]{$name}) ? $_[0]{$name} :
-                ($_[0]{$name} = $_[0]->$builder);
-            } :
-            $default ? sub {
-                $#_ ? $_[0]{$name} = $_[1] :
-                exists($_[0]{$name}) ?  $_[0]{$name} :
-                ($_[0]{$name} = $default->(@_));
-            } :
-            sub {
-                $#_ ? $_[0]{$name} = $_[1] :
-                $_[0]{$name};
-            };
+    # Create/register a metaclass object for the package
+    my $meta = Pegex::Base::Meta::Class->initialize($package);
 
-        $method = trace_accessor_calls($name, $method)
-            if $ENV{PERL_PEGEX_ACCESSOR_CALLS};
+    # Make calling class inherit from Pegex::Object by default
+    extends($meta, 'Pegex::Object');
 
-        *{"$caller\::$name"} = $method;
-    };
-    *{"$caller\::extends"} = sub {
-        eval "require $_[0]";
-        @{"$caller\::ISA"} = $_[0];
-    },
-    @{"$caller\::ISA"} = ('Pegex::Object');
+    # Export the 'has' and 'extends' helper functions
+    _export($package, has => \&has, $meta);
+    _export($package, extends => \&extends, $meta);
 
-    export_xxx($caller) if $ENV{PERL_PEGEX_XXX};
+    # Possibly export some handy debugging stuff
+    _export_xxx($package) if $ENV{PERL_PEGEX_XXX};
 }
 
-sub export_xxx {
-    my ($caller) = @_;
+# Attribute generator
+sub has {
+    my ($meta, $name) = splice(@_, 0, 2);
+    my %args;
+
+    # Support 2-arg shorthand: 
+    #     has foo => 42;
+    if (@_ % 2) {
+        my $default = shift;
+        my $sub = 
+            ref($default) eq 'HASH' ? sub {+{%$default}} :
+            ref($default) eq 'ARRAY' ? sub {[@$default]} :
+            sub {$default};
+        %args = (default => $sub);
+    }
+    %args = (%args, @_);
+
+    # Add attribute to meta class object
+    $meta->add_attribute($name => %args);
+
+    # Make a Setter/Getter accessor
+    my ($builder, $default) = @args{qw(builder default)};
+    my $accessor =
+        $builder ? sub {
+            $#_ ? $_[0]{$name} = $_[1] :
+            exists($_[0]{$name}) ? $_[0]{$name} :
+            ($_[0]{$name} = $_[0]->$builder);
+        } :
+        $default ? sub {
+            $#_ ? $_[0]{$name} = $_[1] :
+            exists($_[0]{$name}) ? $_[0]{$name} :
+            ($_[0]{$name} = $default->($_[0]));
+        } :
+        sub {
+            $#_ ? $_[0]{$name} = $_[1] : $_[0]{$name};
+        };
+
+    # Dev debug thing to trace calls to accessor subs.
+    $accessor = _trace_accessor_calls($name, $accessor)
+        if $ENV{PERL_PEGEX_ACCESSOR_CALLS};
+
+    # Export the accessor.
+    _export($meta->{package}, $name, $accessor);
+}
+
+# Inheritance maker
+sub extends {
+    my ($meta, $parent) = @_;
+    eval "require $parent";
+    no strict 'refs';
+    @{"$meta->{package}\::ISA"} = ($parent);
+}
+
+# Use this for exports and meta-exports
+sub _export {
+    my ($package, $name, $code, $meta) = @_;
+    if (defined $meta) {
+        my $orig = $code;
+        $code = sub {
+            unshift @_, $meta;
+            goto &$orig;
+        };
+    }
+    no strict 'refs';
+    *{"$package\::$name"} = $code;
+}
+
+# Export the 4 debugging subs from XXX.pm
+sub _export_xxx {
+    my ($package) = @_;
     eval "use XXX -with => 'YAML::XS'; 1" or die $@;
     no strict 'refs';
-    *{"$caller\::WWW"} = \&{__PACKAGE__ . '::WWW'};
-    *{"$caller\::XXX"} = \&{__PACKAGE__ . '::XXX'};
-    *{"$caller\::YYY"} = \&{__PACKAGE__ . '::YYY'};
-    *{"$caller\::ZZZ"} = \&{__PACKAGE__ . '::ZZZ'};
+    _export($package, WWW => \&{__PACKAGE__ . '::WWW'});
+    _export($package, XXX => \&{__PACKAGE__ . '::XXX'});
+    _export($package, YYY => \&{__PACKAGE__ . '::YYY'});
+    _export($package, ZZZ => \&{__PACKAGE__ . '::ZZZ'});
 }
 
+# A tracing wrapper for debugging accessors
 my $trace_exclude = +{
     map {($_, 1)} (
-#         'Some::Module some_accessor',
-#         'Some::Module some_other_accessor',
+        'Some::Module some_accessor',
+        'Some::Module some_other_accessor',
     )
 };
-sub trace_accessor_calls {
+sub _trace_accessor_calls {
     require Time::HiRes;
     my ($name, $accessor) = @_;
     sub {
         my ($pkg, $file, $line, $sub) = caller(0);
         unless ($trace_exclude->{"$pkg $name"}) {
             warn "$pkg $name $line\n";
-#             Time::HiRes::usleep(100000);
+            Time::HiRes::usleep(100000);
         }
         goto &$accessor;
     };
 }
 
+# The remainder of this module was heavily inspired by Pegex::Basee, and tried to do
+# what Pegex::Basee does, only much less.
+package Pegex::Base::Meta::Class;
+
+my $meta_class_objects = {};
+
+sub name { $_[0]->{package} }
+
+sub initialize {
+    my ($class, $package) = @_;
+
+    return $meta_class_objects->{$package} //= do {
+        bless {
+            package => $package,
+            attributes => {},
+            _attributes => [],
+        }, $class;
+    };
+}
+
+# Make a new attrribute object and add it to both a hash and an array, so that
+# we can preserve the order defined.
+sub add_attribute {
+    my $self = shift;
+    my ($name, %args) = @_;
+    push @{$self->{_attributes}}, (
+        $self->{attributes}{$name} =
+            bless {
+                name => $name,
+                %args,
+            }, 'Pegex::Base::Meta::Attribute'
+    );
+}
+
+sub new_object {
+    my ($self, $params) = @_;
+    my $object = $self->_construct_instance($params);
+    $object->BUILDALL($params) if $object->can('BUILDALL');
+    return $object;
+}
+
+sub _construct_instance {
+    my ($self, $params) = @_;
+    my $instance = bless {}, $self->name;
+    foreach my $attr ($self->get_all_attributes()) {
+        my $name = $attr->{name};
+        next if exists $instance->{$name};
+        if (exists $params->{$name}) {
+            $instance->{$name} = $params->{$name};
+            next;
+        }
+        if (not $attr->{lazy}) {
+            if (my $builder = $attr->{builder}) {
+                $instance->{$name} = $instance->$builder();
+                next;
+            }
+            elsif (my $default = $attr->{default}) {
+                $instance->{$name} = $default->($instance);
+            }
+        }
+    }
+    return $instance;
+}
+
+sub get_all_attributes {
+    my $self = shift;
+    my (@attrs, %attrs);
+    for my $package (@{mro::get_linear_isa($self->name)}) {
+        my $meta = Pegex::Base::Meta::Class->initialize($package);
+        for my $attr (@{$meta->{_attributes}}) {
+            my $name = $attr->{name};
+            next if $attrs{$name};
+            push @attrs, ($attrs{$name} = $attr);
+        }
+    }
+    return @attrs;
+}
+
 package Pegex::Object;
 
 sub new {
-    my $self = bless {@_[1..$#_]}, $_[0];
-    no strict 'refs';
-    for my $class (@{[@{"$_[0]::ISA"}, $_[0]]}) {
-        if (defined &{"$class\::BUILD"}) {
-            &{"$class\::BUILD"}($self);
-        }
-    }
-    return $self;
+    my $class = shift;
+    my $real_class = Scalar::Util::blessed($class) || $class;
+    my $params = $real_class->BUILDARGS(@_);
+    return Pegex::Base::Meta::Class->initialize($real_class)->new_object($params);
 }
 
-sub BUILD {
-    no strict 'refs';
-    my $meta = ${ref($_[0]) . "::__meta"};
-    for my $has (@{$meta->{has}}) {
-        my ($name, $args) = @$has;
-        next if exists $_[0]->{$name} || $args->{lazy};
-        if (my $default = $args->{default}) {
-            $_[0]->{$name} = $default->($_[0]);
-        }
-        elsif (my $builder = $args->{builder}) {
-            $_[0]->{$name} = $_[0]->$builder();
+sub BUILDARGS {
+    return {@_[1..$#_]};
+}
+
+sub BUILDALL {
+    return unless $_[0]->can('BUILD');
+    my ($self, $params) = @_;
+    for my $package (
+        reverse @{mro::get_linear_isa(Scalar::Util::blessed($self))}
+    ) {
+        no strict 'refs';
+        if (defined &{"$package\::BUILD"}) {
+            &{"$package\::BUILD"}($self, $params);
         }
     }
-    return;
+}
+
+sub dump {
+    no warnings 'once';
+    my $self = shift;
+    require Data::Dumper;
+    local $Data::Dumper::Maxdepth = shift if @_;
+    Data::Dumper::Dumper $self;
+}
+
+sub meta {
+    Pegex::Base::MOP::Class->initialize(Scalar::Util::blessed($_[0]) || $_[0]);
 }
 
 1;
